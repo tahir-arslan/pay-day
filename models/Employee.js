24bcc1029889933766d2aabb3a662950076767ae
ThisOneGuy <tahir.arslan@gmail.com>
2022-08-17 18:33:35 -0400

    debugged

--- models/Employee.js
+++ models/Employee.js
@@ -8,67 +8,64 @@ class Employee extends Model {
     }
 }
 
-Employee.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
+Employee.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    is_manager: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    // email: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false
+    // },
+    hourly_wage: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6]
+        }
+    }
+    // FK_shift: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    //     references: {
+    //         model: 'employee',
+    //         key: 'id'
+    //     }
+    // }
+}, {
+    hooks: {
+        async beforeCreate(employeeData) {
+            employeeData.password = await bcrypt.hash(employeeData.password, 10);
+            return employeeData;
         },
-        is_manager: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        hourly_wage: {
-            type: DataTypes.DECIMAL(10, 2),
-            allowNull: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [6]
-            }
-        },
-        FK_shift: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'employee',
-                key: 'id'
-            }
+        async beforeUpdate(updatedEmployeeData) {
+            updatedEmployeeData.password = await bcrypt.hash(updatedEmployeeData.password, 10);
+            return updatedEmployeeData;
         }
     },
-    {
-        hooks: {
-            async beforeCreate(employeeData) {
-                employeeData.password = await bcrypt.hash(employeeData.password, 10);
-                return employeeData;
-            },
-            async beforeUpdate(updatedEmployeeData) {
-                updatedEmployeeData.password = await bcrypt.hash(updatedEmployeeData.password, 10);
-                return updatedEmployeeData;
-            }
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'employee'
-    }
-);
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'employee'
+});
 
 module.exports = Employee;
